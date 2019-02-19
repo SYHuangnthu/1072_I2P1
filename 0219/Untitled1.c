995d1f95124710d0c1b621c30cd75eeba0ffdb7d
binbinko <43368507+binbinko@users.noreply.github.com>
2019-02-19 21:23:32 +0800

    Add files via upload

--- 0219/Untitled1.c
+++ 0219/Untitled1.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-
+void func1();
+void func2();
 int main(void)
 {
-    int x;
-    printf("Hello,\t world\n"); /*\ 換行 \t tab \"快樂ㄉ雙引號 \\ 顯示反斜線囉*/
-    printf("1234+5678\n");
-    printf("%d\n", 1234+5678);/*%d 十進位數*/
-    printf("%10d\n", 1234+5678);/*位數不夠會自己補上空格喔*/
-    printf("%07d\n", 1234+5678);/*07 空位補0*/
-    scanf("%d", &x);
-    printf("%d", x+1);
+    int x,y;
+//    printf("Hello,\t world\n"); /*\ 換行 \t tab \"快樂ㄉ雙引號 \\ 顯示反斜線囉*/
+//    printf("1234+5678\n");
+//    printf("%d\n", 1234+5678);/*%d 十進位數*/
+//    printf("%10d\n", 1234+5678);/*位數不夠會自己補上空格喔*/
+//    printf("%07d\n", 1234+5678);/*07 空位補0*/
+    scanf("%d %d", &x, &y); //&取得位置
+    printf("%d\n", x*(y+3));
+    func1();
 
     return 0;
+}
+void func1()
+{
+    printf("1\n");
+    func2();
+
+}
+void func2()
+{
+    printf("2\n");
+
 }
