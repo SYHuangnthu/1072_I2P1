cfd6ddc5c74d30a651008d7382192a0cf891ac90
binbinko <43368507+binbinko@users.noreply.github.com>
2019-02-26 20:19:02 +0800

    Add files via upload

--- 0219/Untitled1.c
+++ 0219/Untitled1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int f(void)
-{
-    return 1000;
-}
+
 int main(void)
 {
-    //int x = 100;
-    printf("%10d", f());
+    int x;
+    int y;
+    x = 5;
+    y = 10;
+    y = y/x;//注意y 是int，如果小數就GG
+    printf("%10f", y);
 
     return 0;
 }
