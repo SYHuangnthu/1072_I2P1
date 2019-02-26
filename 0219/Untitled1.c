46ef2e66d7ffc5850ec3caba705dc8c002cc42b3
binbinko <43368507+binbinko@users.noreply.github.com>
2019-02-26 20:25:59 +0800

    Add files via upload

--- 0219/Untitled1.c
+++ 0219/Untitled1.c
@@ -7,8 +7,9 @@ int main(void)
     int y;
     x = 5;
     y = 10;
-    y = y/x;//注意y 是int，如果小數就GG
-    printf("%10f", y);
+    y = (double)y/x + 0.5;//注意y 是int，如果小數就GG
+    //左邊的y還是int，右邊的y改設成double就可以只加0.5就四捨五入囉
+    printf("%10d", y);
 
     return 0;
 }
