b264a000d7fa02c6b31ff06f40c4bf8c20edd207
binbinko <43368507+binbinko@users.noreply.github.com>
2019-04-02 20:42:40 +0800

    Add files via upload

---
+++ 0402/0402.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+//遞迴呼叫
+
+int paren(int n)
+{
+//((((1+2)*3+4)*5+6)*7+8)*9+10=4555
+    int ans;
+
+    if(n>1){
+        printf("(");
+        ans = paren(n-1);
+        if (n%2){
+          printf("*");
+          ans = ans*n;
+        }
+        else{
+            printf("+");
+            ans += n;
+        }
+        printf("%d", n);
+        if (n%2==0) printf(")");
+    }
+    else{
+        printf("1");
+        ans = 1;
+    }
+    return ans;
+}
+
+int main (void)
+{
+    printf(" = %d", paren(9));
+    return 0;
+}
+
+/*
+double power(double n, int p);//記得加分號(((
+
+int main(void)
+{
+    printf("%f\n", power(2.3,5));
+    return 0;
+
+}
+
+宣告 double power(double n, int p); ，然後使用下面的方式來做遞迴:
+(0) 先檢查 p 是否等於 0，是的話就傳回答案 1; 若 p 不等於 0 則先算出 ans = power(n, p/2);
+(1) 如果 p 是偶數，則 return ans*ans;
+(2) 如果 p 是奇數，則 return n*ans*ans;
+這裡有使用到整數除法無條件捨去的特性。
+
+double power(double n, int p)//p>=0
+{
+    double ans;
+    if(p ==0)
+        ans =1;
+    else
+        ans = power(n, p/2);
+    if(p%2==0) return ans*ans;
+    else return n*ans*ans;
+//    if(p>0)
+//        power(n,p-1)*n;
+//    else
+//        return 1;
+}
+*/
+/*
+void f(int x)
+{
+    if(x>0)
+    {
+        printf("before f(%d-1)\n", x);
+        f(x-1);//呼叫f把動作隔成兩部分QQ
+        printf("after f(%d-1)\n", x);
+    }
+}
+
+//想要變成f(5) -> before f(5-1), before f(4-1), f(3),  f(4), after f(5-1)
+
+int main(void)
+{
+    f(5);
+    return 0;
+}
+*/
+
+/*
+int f(int x)
+{
+    if (x>10) return 0;//停止條件
+    return f(2*x)+1;//又呼叫ㄌ一次自己
+}
+//f(3)= f(6)+1 = f(12) +1 +1 =0+1+1
+int main(void)
+{
+    printf("%d\n", f(3));
+    return 0;
+}
+
+*/
