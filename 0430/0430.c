5ce3e2e25471c1dc9ad80a960c37853e61fa2f0b
binbinko <43368507+binbinko@users.noreply.github.com>
2019-04-30 20:26:32 +0800

    Add files via upload

--- 0430/0430.c
+++ 0430/0430.c
@@ -1,8 +1,45 @@
 #include<stdio.h>
 
+int main(void)
+{
+    char* str[100];
+    char* p;
+    printf("%p", str);
+    p=str;//P是指標變數，地址可以改，但是陣列str的門牌號碼不能改
+
+    scanf("%s", str);
+    printf("%c\n", *p);//　*就是開門去拿東西，
+    printf("%s\n", p);
+    printf("%c\n", *(p+1));//加一就是拿下一號的東西
+    return 0;
+//指標跟陣列不一樣!!
+}
+/*
+//本週練習題
+void swap(int* p,int* q)
+{
+    int tmp;
+    tmp=*p;
+    *p=*q;
+    *q=tmp;
+
+}
+int main(void)
+{
+    int x,y;
+    x=5;
+    y=7;
+    printf("%d %d\n",x,y);
+    swap(&x,&y);
+    printf("%d %d\n",x,y);
+    return 0;
+}
+*/
+
+/*
 void f(int* p)//p=&x;
 {
-    *p=*p*2+3;//*p是取儲存在xㄉ值
+    *p=*p*2+3;// *p是取儲存在xㄉ值
 }
 
 int main(void)
@@ -28,3 +65,4 @@ int main(void)
     printf("%d\n",x);
     return 0;
 }
+*/
