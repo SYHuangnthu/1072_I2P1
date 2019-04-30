1c53e1f9b64770b009b0d84c4cf2acd2b963a4e6
binbinko <43368507+binbinko@users.noreply.github.com>
2019-04-30 21:18:51 +0800

    Add files via upload

--- 0430/0430.c
+++ 0430/0430.c
@@ -1,8 +1,93 @@
 #include<stdio.h>
+#include<ctype.h>
 
 int main(void)
 {
-    char* str[100];
+    char str[100];
+    char rts[100];
+    scanf("%s", str);
+    scanf("%s", rts);
+    if(strcmp(str,rts,5)==0) printf("equal\n");
+    return 0;
+
+}
+
+/*
+int main(void)
+{
+    char str[100];
+    char *p;
+    scanf("%s", str);
+
+    p=&str[-1];
+    p =str-1;
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        p[i]=i*i;
+        printf("%c",str[i]);
+//        p[i]==*[p+i]==*(str-1+i)
+    }
+    printf("\n");
+    return 0;
+}
+*/
+
+/*
+
+int main(void)
+{
+    char str[100];
+    char *p;
+    scanf("%s", str);
+    p=&str[1];//取編號ㄉ位置
+    p=str+1;//兩ㄍ意思是一樣ㄉ
+    int i=0;
+    while(str[i]!='\0')
+        //等同於*(str+i)等同於i[str]
+    {
+//        printf("%c", str[i]);
+        printf("%c", toupper(str[i]));
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
+
+*/
+
+/*
+unsigned long mylen1(char* p)
+{
+    char *q;
+    q=p;
+    while(*p!='\0') p=p+1;
+    return p=q;
+}
+unsigned long mylen1(char* p)
+{
+    int i=0;
+    while (*(p+i)!='\0')i++;
+    return i;
+
+}
+
+
+int main(void)
+{
+    char str[100];
+    scanf("%s",str);
+    printf("%p\n",str);
+
+    printf("%lu\n", mylen(str));
+    return 0;
+}
+*/
+
+/*
+int main(void)
+{
+    char str[100];
     char* p;
     printf("%p", str);
     p=str;//P是指標變數，地址可以改，但是陣列str的門牌號碼不能改
@@ -10,10 +95,15 @@ int main(void)
     scanf("%s", str);
     printf("%c\n", *p);//　*就是開門去拿東西，
     printf("%s\n", p);
+    printf("%c %c\n", *p,str[0]);
     printf("%c\n", *(p+1));//加一就是拿下一號的東西
+    printf("%c %c\n",*str,p[0]);
+    printf("%c %c\n",*(str+1),p[1]);
     return 0;
 //指標跟陣列不一樣!!
 }
+*/
+
 /*
 //本週練習題
 void swap(int* p,int* q)
